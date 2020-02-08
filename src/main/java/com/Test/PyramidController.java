e3fa9649a769e5d4c9713c08cad3ac055a9af121
anuragpatro92 <anuragpatro92@gmail.com>
2020-02-07 19:58:18 -0800

    adding pyrmaid code

--- src/main/java/com/Test/PyramidController.java
+++ src/main/java/com/Test/PyramidController.java
@@ -1,24 +1,53 @@
 package com.Test;
 
-
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
-
-@Path("/testservice")
+@Path("/service")
 public class PyramidController {
 
-
 	@GET
-	@Path("/test1")
+	@Path("/checkPyramid")
 	@Produces(MediaType.APPLICATION_JSON)
-	public String getTestService() {
-		return "Hello World! This is coming from webservice!!";
+	public String getTestService(@QueryParam(value = "str") String str) {
+
+		return checkPyramid(str);
+	}
+
+	@SuppressWarnings("rawtypes")
+	private String checkPyramid(String str) {
+
+		HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+
+		for (char c : str.toCharArray()) {
+			map.put(c, map.getOrDefault(c, 0) + 1);
 		}
 
+		Set<Integer> set = new HashSet<Integer>();
+
+		Iterator it = map.entrySet().iterator();
+		int size = map.size();
+		while (it.hasNext()) {
+
+			Map.Entry val = (Entry) it.next();
+			if (set.contains(val.getValue()) || (Integer) val.getValue() > size) {
+				return "false";
+			}
+			set.add((Integer) val.getValue());
+
+		}
 	
-	
+		return "true";
+	}
+
 }
